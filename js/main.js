e0bcb40464596cecf39021225f18d1cb059bf1ce
unknown <host.sf@gmail.com>
2015-07-16 20:52:20 +0300

    contact section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+// Clear placeholder
+	(function() {
+		$('input,textarea').focus(function(){
+				$(this).data('placeholder',$(this).attr('placeholder'))
+				$(this).attr('placeholder','');
+		});
+		$('input,textarea').blur(function(){
+			$(this).attr('placeholder',$(this).data('placeholder'));
+		});
+	}());
+
 // Delay animation for active menu
 	var activeMenu = $('.main-nav__left').find('li:first-child');
 	setTimeout(function(){
